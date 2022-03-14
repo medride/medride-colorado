20a7613596fc7d3a64b7d9888d315db03fe12ac9
Ryan A <ryanastaphan@gmail.com>
2022-03-14 09:44:58 +0700

    About to mess around with Places Autocomplete in an effort to have a pickup and dropoff location.

--- components/costCalculator/map.tsx
+++ components/costCalculator/map.tsx
@@ -8,18 +8,43 @@ type DrectionsResult = google.maps.DirectionsResult
 type MapOptions = google.maps.MapOptions
 
 const Map = () => {
-  const center = useMemo(() => ({ lat: 43, lng: -80 }), [])
+  const [office, setOffice] = useState<LatLngLiteral>()
+  const mapRef = useRef<GoogleMap>()
+  const center = useMemo<LatLngLiteral>(
+    () => ({ lat: 38.2673594, lng: -104.6609081 }),
+    []
+  )
+  const options = useMemo<MapOptions>(
+    () => ({
+      mapId: 'd725b651261adacd',
+      disableDefaultUI: true,
+      clickableIcons: false,
+    }),
+    []
+  )
+  const onLoad = useCallback((map) => (mapRef.current = map), [])
+
   return (
     <div className="flex h-screen">
       <div className="w-1/5 bg-gray-900 p-4">
         <h1>Controls</h1>
+        <Places
+          setOffice={(position) => {
+            setOffice(position)
+            mapRef.current?.panTo(position)
+          }}
+        />
       </div>
       <div className="h-screen w-4/5">
         <GoogleMap
-          zoom={10}
+          zoom={11}
           center={center}
           mapContainerClassName="map-container"
-        />
+          options={options}
+          onLoad={onLoad}
+        >
+          {office && <Marker position={office} />}
+        </GoogleMap>
       </div>
     </div>
   )
--- components/costCalculator/places.tsx
+++ components/costCalculator/places.tsx
@@ -1,5 +1,57 @@
-const Places = () => {
-  return <div>places</div>
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from 'use-places-autocomplete'
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from '@reach/combobox'
+import '@reach/combobox/styles.css'
+
+type PlacesProps = {
+  setOffice: (position: google.maps.LatLngLiteral) => void
+}
+
+const Places = ({ setOffice }: PlacesProps) => {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete()
+
+  const handleSelect = async (val: string) => {
+    setValue(val, false)
+    clearSuggestions()
+
+    const results = await getGeocode({ address: val })
+    const { lat, lng } = await getLatLng(results[0])
+    setOffice({ lat, lng })
+  }
+
+  return (
+    <Combobox onSelect={handleSelect}>
+      <ComboboxInput
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        disabled={!ready}
+        placeholder="Enter your pick-up location"
+        className="w-full p-2"
+      />
+      <ComboboxPopover>
+        <ComboboxList>
+          {status === 'OK' &&
+            data.map(({ place_id, description }) => (
+              <ComboboxOption key={place_id} value={description} />
+            ))}
+        </ComboboxList>
+      </ComboboxPopover>
+    </Combobox>
+  )
 }
 
 export default Places
